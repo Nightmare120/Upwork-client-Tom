34ce3e5141f0b4b232f91ef12df146e46aa78042
manmohit <manmohittaiya123@gmail.com>
2023-08-18 01:29:31 +0530

    done

--- src/api/Constant.js
+++ src/api/Constant.js
@@ -1,10 +1,10 @@
-// export const STRAPI_URL = "https://king-prawn-app-iacmq.ondigitalocean.app";
-export const STRAPI_URL = "http://localhost:1337";
+export const STRAPI_URL = "https://king-prawn-app-iacmq.ondigitalocean.app";
+// export const STRAPI_URL = "http://localhost:1337";
 
-// export const TOKEN =
-//     "586c9ed4fe6f06f4331044ee271f3b571a6ff9cfe48b24ebb8e1ad298636024bf79b15f8f2da1583110210471f92da64cd724e809da7314a6d2f9307e159a699eb2c2d237474e8ddcb420eddae0e8a5c42bea23980b4c9aeee025436698d75d178273a92a1a5f84332eabdfd155280823c8212474a0a2c3fc238d921137668aa";
 export const TOKEN =
-    "be4819630012d9524701d22a7c94aeb051070aa58d75a4dc991e3b841b94c1a21bc2fdadbc5de90b40b06df011c8167d8e4fbf23d8320b2092ac5edd2f98ed81262f65ab08ff82712de460c16e5736d5e318dfd08aae44fedef005af1f2c0a0d5c20be0be9ea5e33bdf38f42d1f9004527b79b715bdb11af33792f7ddcb94446";
+    "586c9ed4fe6f06f4331044ee271f3b571a6ff9cfe48b24ebb8e1ad298636024bf79b15f8f2da1583110210471f92da64cd724e809da7314a6d2f9307e159a699eb2c2d237474e8ddcb420eddae0e8a5c42bea23980b4c9aeee025436698d75d178273a92a1a5f84332eabdfd155280823c8212474a0a2c3fc238d921137668aa";
+// export const TOKEN =
+//     "be4819630012d9524701d22a7c94aeb051070aa58d75a4dc991e3b841b94c1a21bc2fdadbc5de90b40b06df011c8167d8e4fbf23d8320b2092ac5edd2f98ed81262f65ab08ff82712de460c16e5736d5e318dfd08aae44fedef005af1f2c0a0d5c20be0be9ea5e33bdf38f42d1f9004527b79b715bdb11af33792f7ddcb94446";
 
 export const HEADERS = {
     headers: {
